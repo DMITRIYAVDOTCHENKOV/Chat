a0cb257916f3b53b77b91cf73aaab4bfdc5ae584
DMITRIYAVDOTCHENKOV <dimka.a2010@yandex.ru>
2022-04-25 14:05:14 +0300

    Lesson6

--- src/main/java/gb/avdotchenkov/server/MyServer.java
+++ src/main/java/gb/avdotchenkov/server/MyServer.java
@@ -5,30 +5,49 @@ import gb.avdotchenkov.server.authentication.BaseAuthenticationService;
 import gb.avdotchenkov.server.authentication.DBAuthenticationService;
 import gb.avdotchenkov.server.handler.ClientHandler;
 
+import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.*;
 
 public class MyServer {
-	
+
+	public static final Logger logger = Logger.getLogger("server");
 	private final ServerSocket serverSocket;
 	private final AuthenticationService authenticationService;
-	private final List <ClientHandler> clients;
-	
-	public MyServer (int port) throws IOException {
+	private final List<ClientHandler> clients;
+
+	public MyServer(int port) throws IOException {
+
+
+		Handler handler = new FileHandler("src/main/resources/Logger/logs/LogFromLogger.log");
+		logger.addHandler(handler);
+		handler.setLevel(Level.ALL);
+		handler.setFormatter(new SimpleFormatter());
+//		handler.setFormatter(new Formatter() {
+//			@Override public String format (LogRecord record) {
+//				return String.format("%s\t%s\t%s%n", record.getLevel(), new Data(record.getMillis()),
+//				                     record.getMessage());
+//
+//			}
+//		});
+
+
 		serverSocket = new ServerSocket(port);
 		authenticationService = new DBAuthenticationService();
-		clients = new ArrayList <>();
+		clients = new ArrayList<>();
 	}
-	
-	
-	public void start () {
+
+
+	public void start() {
 		authenticationService.startAuthentication();
-		System.out.println("СЕРВЕР ЗАПУЩЕН!");
+		logger.log(Level.SEVERE, "СЕРВЕР ЗАПУЩЕН");
+
 		System.out.println("----------------");
-		
+
 		try {
 			while (true) {
 				waitAndProcessNewClientConnection();
@@ -39,29 +58,28 @@ public class MyServer {
 			authenticationService.endAuthentication();
 		}
 	}
-	
-	private void waitAndProcessNewClientConnection () throws IOException {
-		System.out.println("Ожидание клиента...");
+
+	private void waitAndProcessNewClientConnection() throws IOException {
+		logger.log(Level.SEVERE, "ОЖИДАНИЕ КЛИЕНТА!");
 		Socket socket = serverSocket.accept();
-		System.out.println("Клиент подключился!");
-		
+		logger.log(Level.SEVERE, "Клиент подключился!");
 		processClientConnection(socket);
 	}
-	
-	private void processClientConnection (Socket socket) throws IOException {
+
+	private void processClientConnection(Socket socket) throws IOException {
 		ClientHandler handler = new ClientHandler(this, socket);
 		handler.handle();
 	}
-	
-	public synchronized void subscribe (ClientHandler clientHandler) {
+
+	public synchronized void subscribe(ClientHandler clientHandler) {
 		clients.add(clientHandler);
 	}
-	
-	public synchronized void unSubscribe (ClientHandler clientHandler) {
+
+	public synchronized void unSubscribe(ClientHandler clientHandler) {
 		clients.remove(clientHandler);
 	}
-	
-	public synchronized boolean isUsernameBusy (String username) {
+
+	public synchronized boolean isUsernameBusy(String username) {
 		for (ClientHandler client : clients) {
 			if (client.getUsername().equals(username)) {
 				return true;
@@ -69,12 +87,12 @@ public class MyServer {
 		}
 		return false;
 	}
-	
-	public AuthenticationService getAuthenticationService () {
+
+	public AuthenticationService getAuthenticationService() {
 		return authenticationService;
 	}
-	
-	public synchronized void broadcastMessage (String message, ClientHandler sender, boolean isServerMessage)
+
+	public synchronized void broadcastMessage(String message, ClientHandler sender, boolean isServerMessage)
 			throws IOException {
 		for (ClientHandler client : clients) {
 			if (client == sender) {
@@ -83,12 +101,14 @@ public class MyServer {
 			client.sendMessage(isServerMessage ? null : sender.getUsername(), message);
 		}
 	}
-	
-	public synchronized void broadcastMessage (String message, ClientHandler sender) throws IOException {
+
+	public synchronized void broadcastMessage(String message, ClientHandler sender) throws IOException {
 		broadcastMessage(message, sender, false);
+		sender.chatHistory(message, sender);
+
 	}
-	
-	public synchronized void sendPrivateMessage (ClientHandler sender, String recipient, String privateMessage)
+
+	public synchronized void sendPrivateMessage(ClientHandler sender, String recipient, String privateMessage)
 			throws IOException {
 		for (ClientHandler client : clients) {
 			if (client.getUsername().equals(recipient)) {
@@ -96,26 +116,29 @@ public class MyServer {
 			}
 		}
 	}
-	
-	public synchronized void broadcastClients (ClientHandler sender) throws IOException {
+
+	public synchronized void broadcastClients(ClientHandler sender) throws IOException {
 		for (ClientHandler client : clients) {
-			
+
 			client.sendServerMessage(String.format("%s присоединился к чату", sender.getUsername()));
 			client.sendClientsList(clients);
 		}
 	}
-	
-	public synchronized void broadcastClientDisconnected (ClientHandler sender) throws IOException {
+
+	public synchronized void broadcastClientDisconnected(ClientHandler sender) throws IOException {
 		for (ClientHandler client : clients) {
 			if (client == sender) {
 				continue;
 			}
 			client.sendServerMessage(String.format("%s отключился", sender.getUsername()));
 			client.sendClientsList(clients);
-			
+
 		}
 	}
-	
-	public void broadcastMessage (String format) {
+
+	public void broadcastMessage(String format) {
+	}
+
+	public void updateUsername(ClientHandler clientHandler, String newUsername) {
 	}
 }
--- src/main/java/gb/avdotchenkov/server/authentication/DBAuthenticationService.java
+++ src/main/java/gb/avdotchenkov/server/authentication/DBAuthenticationService.java
@@ -2,19 +2,22 @@ package gb.avdotchenkov.server.authentication;
 
 import java.sql.*;
 
+import static gb.avdotchenkov.server.MyServer.logger;
+
 
 public class DBAuthenticationService implements AuthenticationService {
-	
+
 	private static final String SQLAUTH_BD = "jdbc:sqlite:src/main/resources/bd/AuthenticationBD.db";
 	private static Statement stmt;
 	private static ResultSet rs;
 	private static Connection connection;
-	
-	
-	@Override public String getUsernameByLoginAndPassword (String login, String password) {
+
+
+	@Override
+	public String getUsernameByLoginAndPassword(String login, String password) {
 		String passwordDB = null;
 		String username = null;
-		
+
 		try {
 			PreparedStatement pstmt = connection.prepareStatement("SELECT * FROM auth WHERE login = ?");
 			pstmt.setString(1, login);
@@ -22,39 +25,42 @@ public class DBAuthenticationService implements AuthenticationService {
 			if (rs.isClosed()) {
 				return null;
 			}
-			
+
 			username = rs.getString("username");
 			passwordDB = rs.getString("password");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 		return ((passwordDB != null) && (passwordDB.equals(password))) ? username : null;
 	}
-	
-	@Override public void createUser (String login, String password, String username) {
+
+	@Override
+	public void createUser(String login, String password, String username) {
 		try {
 			PreparedStatement pstmt =
 					connection.prepareStatement("INSERT INTO auth (login, password, username) VALUES (?, ?, ?)");
-			
+
 			pstmt.setString(1, login);
 			pstmt.setString(2, password);
 			pstmt.setString(3, username);
-			
+
 			pstmt.addBatch();
-			
+
 			pstmt.executeBatch();
 		} catch (SQLException e) {
 			e.printStackTrace();
+			logger.warning(e.getMessage());
 		}
 	}
 
 //	@Override public void updateUsername (String login, String newUsername) throws SQLException {
 //
 //	}
-	
-	
-	@Override public Boolean checkLoginByFree (String login) {
+
+
+	@Override
+	public Boolean checkLoginByFree(String login) {
 		String username = null;
 		try {
 			PreparedStatement pstmt = connection.prepareStatement("SELECT * FROM auth WHERE login = ?");
@@ -63,32 +69,38 @@ public class DBAuthenticationService implements AuthenticationService {
 			if (rs.isClosed()) {
 				return true;
 			}
-			
+
 			username = rs.getString("username");
 		} catch (SQLException e) {
 			e.printStackTrace();
+			logger.warning(e.getMessage());
 		}
-		
+
 		return username == null;
 	}
-	
-	@Override public void startAuthentication () {
+
+	@Override
+	public void startAuthentication() {
 		try {
 			Class.forName("org.sqlite.JDBC");
 			connection = DriverManager.getConnection(SQLAUTH_BD);
 			stmt = connection.createStatement();
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
+			logger.warning(e.getMessage());
 		} catch (SQLException e) {
 			e.printStackTrace();
+			logger.warning(e.getMessage());
 		}
 	}
-	
-	@Override public void endAuthentication () {
+
+	@Override
+	public void endAuthentication() {
 		try {
 			connection.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
+			logger.warning(e.getMessage());
 		}
 	}
 }
